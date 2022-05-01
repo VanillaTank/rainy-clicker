14aeaa6725942d6e0e8166c5267ac28e834e6544
VanillaTank <tedien2016@yandex.ru>
2022-05-02 03:43:34 +0800

    add audio player logic

---
+++ js/Music.js
@@ -0,0 +1,154 @@
+class Music {
+
+    // playlist = [
+    //     { title: '5-am_by_danyvin', id: 'song1', path: '../sounds/5-am_by_danyvin.mp3' },
+    //     { title: 'barradeen-the-girl-i-havent-met', id: 'song2', path: '../sounds/barradeen-the-girl-i-havent-met.mp3' },
+    //     { title: 'coffee-time_by_pure', id: 'song3', path: '../sounds/coffee-time_by_pure.mp3' },
+    //     { title: 'ghostrifter-back-home', id: 'song4', path: '../sounds/ghostrifter-back-home.mp3' },
+    //     { title: 'ghostrifter-subtle-break', id: 'song5', path: '../sounds/ghostrifter-subtle-break.mp3' },
+    //     { title: 'le-gang-pain-away', id: 'song6', path: '../sounds/le-gang-pain-away.mp3' },
+    //     { title: 'small-planets_by_finval', id: 'song7', path: '../sounds/small-planets_by_finval.mp3' },
+    //     { title: 'stellar-sky_by_ahoami', id: 'song8', path: '../sounds/stellar-sky_by_ahoami.mp3' },
+    //     { title: 'unreal-rest_by_roman-pchela', id: 'song9', path: '../sounds/unreal-rest_by_roman-pchela.mp3' },
+
+    //     // {title: 'ffgdf', id: '9405943', path: '../sounds/'},
+    // ]
+
+    playlist = [
+        { title: '0', id: 'song0', path: '../sounds/5-am_by_danyvin.mp3' },
+        { title: '1', id: 'song1', path: '../sounds/barradeen-the-girl-i-havent-met.mp3' },
+        { title: '2', id: 'song2', path: '../sounds/coffee-time_by_pure.mp3' },
+        { title: '3', id: 'song3', path: '../sounds/ghostrifter-back-home.mp3' },
+        { title: '4', id: 'song4', path: '../sounds/ghostrifter-subtle-break.mp3' },
+        { title: '5', id: 'song5', path: '../sounds/le-gang-pain-away.mp3' },
+        { title: '6', id: 'song6', path: '../sounds/small-planets_by_finval.mp3' },
+        { title: '7', id: 'song7', path: '../sounds/stellar-sky_by_ahoami.mp3' },
+        { title: '8', id: 'song8', path: '../sounds/unreal-rest_by_roman-pchela.mp3' },
+    ]
+
+    autoPlayTimeout = null;
+    currentAudioHTMLElement = document.querySelector('#selectedSong');
+    currentTrackIndex = null;
+    isSwapModeOn = null;
+
+    getPlaylist = () => {
+        let result = '<ul  class="songs-menu">';
+
+        this.playlist.map((el) => {
+            let active = '';
+
+            if (document.querySelector('#selectedSong').getAttribute('src') === el.path
+                &&
+                !document.querySelector('#selectedSong').paused
+            ) {
+                active = 'active';
+            }
+
+            result += ` <li class="songs-item ${active}" id="${el.id}">
+        <button class="songs-item-start-btn resetBtn">
+        <svg class="play-icon" width="20" height="22" viewBox="0 0 20 22" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0.231225 2.44528C0.231225 0.911663 1.8971 -0.0441679 3.23353 0.722638L18.3459 9.39366C19.6854 10.1623 19.6893 12.0842 18.3529 12.851L3.24055 21.522C1.90411 22.2889 0.231224 21.3245 0.231224 19.7873L0.231225 2.44528Z"/>
+        </svg>        
+        <svg class="pause-icon" width="14" height="21" viewBox="0 0 14 21" xmlns="http://www.w3.org/2000/svg">
+            <rect width="4" height="21" rx="2" />
+            <rect x="10" width="4" height="21" rx="2"/>
+        </svg>        
+        </button>
+        ${el.title}
+      </li>`
+        })
+
+        result += '</ul>';
+
+        return result;
+    }
+
+    setSelectedAudioPath(id) {
+        const selectedSong = this.playlist.filter(el => el.id === id);
+        const path = selectedSong[0].path;
+
+        if (path === this.currentAudioHTMLElement.getAttribute('src')) { return }
+
+        this.currentAudioHTMLElement.setAttribute("src", path);
+    }
+
+    switchTrackByIndex(index) {
+        this.currentAudioHTMLElement.setAttribute("src", this.playlist[index].path);
+        this.currentAudioHTMLElement.currentTime = 0;
+        this.toggleStateSong();
+    }
+
+    toggleStateSong() {
+        if (this.currentAudioHTMLElement.paused) {
+            this.currentAudioHTMLElement.play();
+
+            console.log(`this.isSwapModeOn: ${this.isSwapModeOn}`);
+            
+            this.autoPlayTimeout = setInterval(() => {
+
+                let audioTime = Math.round(this.currentAudioHTMLElement.currentTime);
+
+                let audioLength = Math.round(this.currentAudioHTMLElement.duration);
+
+                document.querySelector(".time").style.width = (audioTime * 100) / audioLength + '%';
+
+                if(audioTime === audioLength && this.isSwapModeOn) {
+                    let nextTrackID = this.getRandomID();
+                    console.log(`nextTrackID: ${nextTrackID}`);
+                    this.setSelectedAudioPath(nextTrackID);
+                    this.toggleStateSong();
+                    clearInterval(this.autoPlayTimeout);
+                }
+
+                if (audioTime === audioLength && this.currentTrackIndex < this.playlist.length - 1 && !this.isSwapModeOn) {
+                    this.currentTrackIndex = this.currentTrackIndex + 1;
+                    console.log(`this.currentTrackIndex: ${this.currentTrackIndex}`);
+                    this.switchTrackByIndex(this.currentTrackIndex);
+                    clearInterval(this.autoPlayTimeout);
+                }
+                else if (audioTime === audioLength && this.currentTrackIndex >= this.playlist.length - 1 && !this.isSwapModeOn) {
+                    this.currentTrackIndex = 0;
+                    console.log(`this.currentTrackIndex: ${this.currentTrackIndex}`);
+                    this.switchTrackByIndex(this.currentTrackIndex);
+                    clearInterval(this.autoPlayTimeout);
+                }
+            }, 60)
+        } else {
+            this.currentAudioHTMLElement.pause();
+            clearInterval(this.autoPlayTimeout);
+        }
+    }
+
+    getRandomID() {
+        let index = Math.floor(Math.random() * this.playlist.length);
+        return this.playlist[index].id;
+    }
+
+    setVolume(volume) {
+        document.querySelector('#selectedSong').volume = volume;
+       this.changeSettings('volume', volume);
+    }
+
+    changePoint(selectedTime, fullWidth) {
+        let selectedProcent = Math.round(selectedTime / fullWidth * 100);
+        let targetTime = document.querySelector('#selectedSong').duration * selectedProcent / 100;
+        document.querySelector('#selectedSong').currentTime = targetTime;
+        document.querySelector(".time").style.width = selectedProcent + '%';
+    }
+
+    onSwapModeHandler() {
+        this.isSwapModeOn = !this.isSwapModeOn;
+        this.changeSettings('isSwapModeOn', this.isSwapModeOn);
+    }
+
+    changeSettings(param, value) {
+        let settings = JSON.parse(localStorage.getItem('rainy-clicker-settings'));
+        settings.audio[param] = value;
+        localStorage.setItem('rainy-clicker-settings', JSON.stringify(settings));
+    }
+
+    setSwapMode(bool) {
+        this.isSwapModeOn = bool;
+        this.changeSettings('isSwapModeOn', this.isSwapModeOn);
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,192 @@
+window.onload = () => {
+
+    const music = new Music();
+    let prevSongID = null;
+
+    const audioTrack = document.querySelector('.audio-track');
+    const volume = document.querySelector('.volume');
+    const swapBtn = document.querySelector('.swap');
+
+
+    if (!isSettingsInLocalStorage()) {
+        showGreetingMsg();
+        setDefaultSettings();
+        music.setVolume(0.5);
+        music.setSwapMode(false);
+    } else {
+        const settings = JSON.parse(localStorage.getItem('rainy-clicker-settings'));
+        changeStyle(settings.theme);
+        music.setVolume(settings.audio.volume);
+        music.setSwapMode(settings.audio.isSwapModeOn);
+    }
+
+    audioTrack.addEventListener('click', (event) => { music.changePoint(event.offsetX, audioTrack.offsetWidth) });
+    volume.addEventListener('input', () => { music.setVolume(volume.value / 100) });
+    swapBtn.addEventListener('click', () => music.onSwapModeHandler());
+
+    document.querySelector('#settingsBtn').addEventListener('click', showSettings);
+
+    document.querySelector('#toggle-music-btn').addEventListener('click', toggleMusic);
+
+    const id = music.getRandomID();
+    music.setSelectedAudioPath(id);
+
+
+    // -----------------------------------------------------------------------------------
+
+    function isSettingsInLocalStorage() {
+        if (!localStorage.getItem('rainy-clicker-settings')) {
+            return false;
+        }
+        return true;
+    }
+
+    function showGreetingMsg() {
+        const div = document.createElement('div');
+        div.classList.add('greeting', 'forest');
+        div.innerHTML = `
+    <div class='greeting-body'>
+       <p>
+         Ты устал и надо расслабиться. <br>
+         Слушай lofi-музыку, шум дождя и неспешно кликай мышкой (скорость можно настроить в SETTINGS).
+       </p>
+       <br>
+       <p>
+         KEEP CALM И ЛОПАЙ ПУЗЫРИКИ
+        </p>
+       <button class='btn greetingBtn' id="greetingBtn" type='button'>
+         OK
+       </button>
+    </div>
+    `;
+
+        document.body.append(div);
+
+        document.querySelector('#greetingBtn').addEventListener('click', () => {
+            div.remove();
+        })
+    }
+
+    function showSettings() {
+
+        const div = document.createElement('div');
+        div.classList.add('settings');
+        div.innerHTML = `
+    <div class='settings-body'>
+     <div class='settings-body-scroll-wrap'>
+     <button class='closeBtn closeSettingsBtn' id="closeSettingsBtn" type='button'>
+     <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+         <rect y="21.2132" width="30" height="3" transform="rotate(-45 0 21.2132)"/>
+         <rect x="21.2131" y="23.3345" width="30" height="3" transform="rotate(-135 21.2131 23.3345)"/>
+     </svg>
+    </button>
+
+    <div class="settings-section">
+         <div class="settings-section-title">Выбор фона</div>
+         <div class="settings-bgc">
+           <button class="theme-thumb" id="forest" type="button"></button>
+           <button class="theme-thumb" id="window" type="button"></button>
+           <button class="theme-thumb" id="leaves" type="button"></button>
+         </div>
+    </div>
+
+    <div class="settings-section">
+         <div class="settings-section-title">Скорость</div>
+         <div class="settings-game-speed">
+           <input class="speed_slider" type="range" min="1" max="8" step="1" value="4">
+         </div>
+    </div>
+
+    <div class="settings-section">
+         <div class="settings-section-title">Анимация дождя</div>
+         <div class="settings-rain-animation">
+             <button class="btn" id="rainOnBtn" type="button">ВКЛ.</button>
+             <button class="btn" id="rainOffBtn" type="button">ВЫКЛ.</button>
+         </div>
+    </div>
+
+    <div class="settings-section">
+        <div class="settings-section-title">Playlist</div>
+          ${music.getPlaylist()}
+        </div>
+       </div>
+    </div>`;
+
+        document.querySelector('.theme').append(div);
+
+        document.querySelector('#closeSettingsBtn').addEventListener('click', () => {
+            div.remove();
+        })
+
+        settingsHandlers();
+    }
+
+    function settingsHandlers() {
+        const settings = JSON.parse(localStorage.getItem('rainy-clicker-settings'));
+
+        document.querySelector(`#${settings.theme}`).classList.add('active');
+
+        const themeBtns = [...document.querySelectorAll('.theme-thumb')];
+        themeBtns.forEach(btn => {
+            btn.addEventListener('click', (evt) => {
+                themeBtns.forEach(btn => btn.classList.remove('active'));
+                document.querySelector(`#${evt.target.id}`).classList.add('active');
+                setSettings("theme", evt.target.id, settings);
+                changeStyle(evt.target.id);
+            })
+        });
+
+        const musicPlayBtns = [...document.querySelectorAll('.songs-item-start-btn')];
+        musicPlayBtns.forEach((el) => {
+            el.addEventListener('click', (event) => {
+                const id = event.target.closest('li').id;
+                music.setSelectedAudioPath(id);
+                music.toggleStateSong();
+
+                if (prevSongID !== null && id !== prevSongID) {
+                    document.querySelector(`#${prevSongID}`).classList.remove('active');
+                }
+
+                event.target.closest('li').classList.toggle('active');
+                document.querySelector('#toggle-music-btn').classList.toggle('active');
+                prevSongID = id;
+            })
+        })
+    }
+
+    function changeStyle(newTheme) {
+        document.querySelector('.theme').classList.remove('forest', 'window', 'leaves');
+        document.querySelector('.theme').classList.add(newTheme);
+    }
+
+    function setSettings(param, value, settings) {
+        settings[param] = value;
+        localStorage.setItem('rainy-clicker-settings', JSON.stringify(settings));
+    }
+
+    function setDefaultSettings() {
+        const settings = {
+            theme: "leaves",
+            speed: 0,  //TODO
+            rainAnimation: false,
+            audio: {
+                volume: 0.5,
+                isRepeatModeOn: false,
+                isSwapModeOn: false
+            }
+        };
+
+        localStorage.setItem('rainy-clicker-settings', JSON.stringify(settings))
+    }
+
+    function toggleMusic() {
+        if (document.querySelector('#toggle-music-btn').classList.contains('active')) {
+            document.querySelector('#toggle-music-btn').classList.remove('active');
+            music.toggleStateSong();
+            return;
+        }
+
+        document.querySelector('#toggle-music-btn').classList.add('active');
+        music.toggleStateSong();
+    }
+}
